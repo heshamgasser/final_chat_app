d0534f83d222bab1289e79d5ed996c5a81c9d3da
Hesham Gasser <heshamgasser32@gmail.com>
2024-01-29 20:02:47 +0300

    validate log in screen

--- lib/views/log_in_screen.dart
+++ lib/views/log_in_screen.dart
@@ -10,81 +10,88 @@ class LogIn extends StatelessWidget {
 
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 20.w),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(height: 150.h),
-              SizedBox(
-                width: 150.w,
-                height: 150.h,
-                child: Image.asset(
-                  'assets/images/scholar.png',
-                ),
-              ),
-              SizedBox(height: 5.h),
-              Center(
-                child: Text(
-                  'Scholar Chat',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-              ),
-              SizedBox(height: 20.h),
-              Text('Sign In', style: Theme.of(context).textTheme.bodyLarge),
-              SizedBox(height: 20.h),
-              CustomTextFormField(
-                  controller: emailController,
-                  labelText: 'Email',
-                  textInputType: TextInputType.emailAddress),
-              SizedBox(height: 20.h),
-              CustomTextFormField(
-                  controller: passwordController,
-                  labelText: 'Password',
-                  textInputType: TextInputType.visiblePassword),
-              SizedBox(height: 20.h),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                ),
-                onPressed: () {},
-                child: Text(
-                  'Sign In',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ),
-              SizedBox(height: 10.h),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't Have an Account? ",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyMedium!
-                        .copyWith(color: Colors.white),
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 20.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 150.h),
+                SizedBox(
+                  width: 150.w,
+                  height: 150.h,
+                  child: Image.asset(
+                    'assets/images/scholar.png',
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, SignUp.routeName);
-                    },
-                    child: Text(
-                      "Sign Up",
+                ),
+                SizedBox(height: 5.h),
+                Center(
+                  child: Text(
+                    'Scholar Chat',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                ),
+                SizedBox(height: 20.h),
+                Text('Sign In', style: Theme.of(context).textTheme.bodyLarge),
+                SizedBox(height: 20.h),
+                CustomTextFormField(
+                    controller: emailController,
+                    labelText: 'Email',
+                    textInputType: TextInputType.emailAddress),
+                SizedBox(height: 20.h),
+                CustomTextFormField(
+                    controller: passwordController,
+                    labelText: 'Password',
+                    textInputType: TextInputType.visiblePassword),
+                SizedBox(height: 20.h),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                  ),
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {}
+                  },
+                  child: Text(
+                    'Sign In',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+                SizedBox(height: 10.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't Have an Account? ",
                       style: Theme.of(context)
                           .textTheme
                           .bodyMedium!
-                          .copyWith(color: Colors.greenAccent),
+                          .copyWith(color: Colors.white),
                     ),
-                  )
-                ],
-              ),
-            ],
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(
+                            context, SignUp.routeName);
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .copyWith(color: Colors.greenAccent),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
